65d4019c972e73b06bb7e2a4f9fb233cbffea420
gtrakas1 <gtrakas23@gmail.com>
2020-09-04 09:38:46 -0500

    Added a user authentication with firebase and a sign-up page

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,6 +17,32 @@ const config =
     measurementId: "G-3ZNLLZHSN6"
 };
 
+export const createUserProfileDocument = async (userAuth, data) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapshot = await userRef.get()
+    if (!snapshot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...data
+            })
+
+        } catch (error) {
+
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+
+}
 
 firebase.initializeApp(config);
 
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './sign-in-and-sign-up.scss'
 import Signin from '../../components/sign-in/sign-in'
+import Signup from '../../components/sign-up/sign-up'
 
-const SignInSignUp = () =>(
+const SignInSignUp = () => (
     <div className='sign-in-and-sign-up'>
-    <Signin/>
+        <Signin />
+        <Signup />
     </div>
 )
 
