3809dbf9496dc9f62860c32339c96c5ed785cbe5
gtrakas1 <gtrakas23@gmail.com>
2020-09-22 14:47:36 -0600

    commit

--- src/components/preview_collection/preview_collection.jsx
+++ src/components/preview_collection/preview_collection.jsx
@@ -8,11 +8,12 @@ const PreviewCollection = ({ title, items }) => (
     <div className='preview-collection'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {
-                items.filter((item, idx) => idx < 4)
-                    .map(item => (
-                        <CollectionItem key={item.id} item={item} />
-                    ))
+            {items
+                .filter((item, idx) => idx < 4)
+                .map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))
+
             }
         </div>
     </div>)
